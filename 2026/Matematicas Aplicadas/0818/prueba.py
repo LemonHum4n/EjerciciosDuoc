41b9e918c83a7d2256bc9a342968c20b799d2ddd
Noa Aguilera <yis.aguilera@duocuc.cl>
2026-08-18 14:19:07 -0400

    0818

---
+++ 2026/Matematicas Aplicadas/0818/prueba.py
@@ -0,0 +1,6 @@
+def k(t:int):
+    return 1.85*t
+
+print("¿Cuantos metros de cable se instalaron al transcurrir 148 horas?")
+print(f"Respuesta: {k(148)} km")
+print(f"¿Y transcurridas 2300 horas? {k(2300)} km")
